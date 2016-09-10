2b585675aed14eae0633663da87d2e4aa2946392
karen818 <klc0818@outlook.com>
2016-09-10 14:10:28 -0500

    adds countdown test

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -10,4 +10,28 @@ describe('Countdown', () => {
   it('should exist', () => {
       expect(Countdown).toExist();
   });
+
+  describe('handleSetCountdown', () => {
+    it('should set state to started and count down', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(10);
+
+      expect(countdown.state.count).toBe(10);
+      expect(countdown.state.countdownStatus).toBe('started');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(9);
+        done();
+      }, 1001);
+    });
+    it('should not count down below 0', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(1);
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        done();
+      }, 3001);
+    });
+  });
 });
