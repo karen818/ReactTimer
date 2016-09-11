702ca4260f5b4cff2bed0f8f13446381f71b93b1
karen818 <klc0818@outlook.com>
2016-09-11 11:58:14 -0500

    support for jquery and foundation added

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -2,8 +2,7 @@ var React = require ('react');
 
 var Controls = React.createClass({
   propTypes: {
-    countdownStatus: React.PropTypes.string,
-    timerStatus: React.PropTypes.string,
+    countdownStatus: React.PropTypes.string.isRequired,
     onStatusChange: React.PropTypes.func.isRequired
   },
   onStatusChange: function(newStatus){
